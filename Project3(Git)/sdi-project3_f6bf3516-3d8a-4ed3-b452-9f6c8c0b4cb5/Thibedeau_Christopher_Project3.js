f92d55a33e63eaafa922f1f40bb53d2eb91dd1c4
Christopher Thibedeau <cthibedeau@gmail.com>
2014-12-11 16:25:07 -0800

    Input information for json file, I will be using this to be able to put in the days of the week for the project. This should fulfill the json requirement.

--- Project3(Git)/sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Thibedeau_Christopher_Project3.js
+++ Project3(Git)/sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Thibedeau_Christopher_Project3.js
@@ -1,7 +1,14 @@
 //alert("JavaScript works!");
 
-//Thibedeau, Christopher
-//Project 3
-//12 11
-//SDI 1411
+// Thibedeau, Christopher
+// Project 3
+// 12 11
+// SDI 1411
+
+// Global Var's
+
+var isItSaturday = true;
+
+
+
 
---
+++ Project3(Git)/sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/jsonFile.js
@@ -0,0 +1,7 @@
+var daysOfTheWeek[]; {
+	
+	"day1" : "Wednesday",
+	"day2" : "Thursday",
+	"day3" : "Firday",
+	"day4" : "Saturday"
+}:
