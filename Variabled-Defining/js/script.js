f22bfef25dc6ca8c0a554a8bd2e9d00c5fcf3977
Chris Thibedeau <cthibedeau@gmail.com>
2015-02-11 21:55:22 -0800

    Assiginement Varriables videos

---
+++ Variabled-Defining/js/script.js
@@ -0,0 +1,29 @@
+// Defining Variables
+
+//var age; //declaration
+//age = 33; //definition
+
+//storage container = value
+
+//var yearOfBirth = 1970; //declaring and defining on the same line
+
+/*
+
+
+var age = 38; // Number variable
+
+var isStudent = true;
+
+var firstName = "Chris";
+
+var phrase =  'I don\' know \n You\'ll have to speak with the professor';
+ */
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+console.log(muppetNames[1]); //lets me see whats in the index #1
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+console.log(muppetNames[1]);
+muppetNames[4] = "Fozzy";
+console.log(muppetNames[4]);
