364d9f8cb2221c2793e2238abf28833b98003ba3
Chris Thibedeau <cthibedeau@gmail.com>
2015-02-12 20:38:47 -0800

    Tested code as well as made changes as needed, converting all of the information in the prompts to actual numbers.

--- Thibedeau_Christopher_Expressions_Assignment./js/script.js
+++ Thibedeau_Christopher_Expressions_Assignment./js/script.js
@@ -9,7 +9,7 @@ var lenghtOfTimeToBrainStorm = prompt("How long did you spend brain storming for
 var lenghtOfTimeToCode = prompt("How much time did you spend actually coding the assignment? \n In minutes please.");
 
 //Variable to add up all of the time spent in minutes to do the assignment.
-var totalTimeSpentOnAssignment  = lengthOfTimeStudying + lenghtOfTimeToBrainStorm + lenghtOfTimeToCode;
+var totalTimeSpentOnAssignment  = Number(lengthOfTimeStudying) + Number(lenghtOfTimeToBrainStorm) + Number(lenghtOfTimeToCode);
 
 //Returning the total time in minutes to do the assignment
 console.log("You spent a total of " + totalTimeSpentOnAssignment + " mins working on your assignment");
@@ -18,7 +18,7 @@ console.log("You spent a total of " + totalTimeSpentOnAssignment + " mins workin
 var testArray = [lenghtOfTimeToCode, lenghtOfTimeToBrainStorm, lengthOfTimeStudying];
 
 //Dividing the total time in an array format by the Number of days we had.
-var arrayCalculation = (testArray[0] + testArray[1] + testArray[2]) / 7;
+var arrayCalculation = (Number(testArray[0]) + Number(testArray[1]) + Number(testArray[2])) / 7;
 
 //Returning an average of time spent each day of the week till the assignment was due.
 console.log("Your overall average time spent per day in minutes is " + arrayCalculation + ".");
