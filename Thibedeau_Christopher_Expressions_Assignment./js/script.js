cf7f0111fbed4b9106b003ef34dd778e00d27983
Chris Thibedeau <cthibedeau@gmail.com>
2015-02-12 20:30:55 -0800

    Setting up the initial variables as well as the first two console logs.

--- Thibedeau_Christopher_Expressions_Assignment./js/script.js
+++ Thibedeau_Christopher_Expressions_Assignment./js/script.js
@@ -3,3 +3,15 @@
 //Expressions assignment
 //SDI 0215 01
 
+
+var lengthOfTimeStudying = prompt("How long did you take to study for the assignment? \n In minutes please.");
+var lenghtOfTimeToBrainStorm = prompt("How long did you spend brain storming for the assignment? \n In minutes please.");
+var lenghtOfTimeToCode = prompt("How much time did you spend actually coding the assignment? \n In minutes please.");
+var totalTimeSpentOnAssignment  = lengthOfTimeStudying + lenghtOfTimeToBrainStorm + lenghtOfTimeToCode;
+
+console.log("You spent a total of " + totalTimeSpentOnAssignment + " mins working on your assignment");
+
+var testArray = [lenghtOfTimeToCode, lenghtOfTimeToBrainStorm, lengthOfTimeStudying];
+var arrayCalculation = (testArray[0] + testArray[1] + testArray[2]) / totalTimeSpentOnAssignment
+
+console.log("Your overall average time spent was " + arrayCalculation + ".");
