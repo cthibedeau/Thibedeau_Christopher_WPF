47a64268d49dc2810f1cb547afe337731b13ed13
cthibedeau <cthibedeau@gmail.com>
2015-02-20 15:41:07 -0800

    no message

---
+++ Functions/js/script.js
@@ -0,0 +1,16 @@
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+var width = 5;
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    //console.log(area);
+}
+console.log(width);
+calcArea();
