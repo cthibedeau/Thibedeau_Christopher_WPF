6395d184cd230cfa80bbfacbcb4e9d2f14f06421
cthibedeau <cthibedeau@gmail.com>
2015-02-24 13:18:19 -0800

    Creating initial variables for the code.

--- Thibedeau_Christopher_Functions_Assignment/js/script.js
+++ Thibedeau_Christopher_Functions_Assignment/js/script.js
@@ -3,3 +3,9 @@
 //Function Assignment
 //SDI 0215
 
+//Variables
+
+var playersName = prompt("Welcome to my store, what is your name?"); //Asks the player's name
+var userAge = prompt("How old are you, sorry have to make sure your old enough to play."); //Used to verify age of the user
+var powerBallChoice = prompt("Would you like numbers for Powerball, \nor for Florida State Lottery?"); //Ask's if the player which version of lotto the player would like.
+
