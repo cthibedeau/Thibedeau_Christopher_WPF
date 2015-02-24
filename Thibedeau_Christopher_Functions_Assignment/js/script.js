38dcc9ea7c1bf2ce772f7e6e86790e33c834339a
cthibedeau <cthibedeau@gmail.com>
2015-02-24 15:13:11 -0800

    Added in functionality that checks there person's age, as well as comments to explain the code. I also changed the userAge var to playersAge in order to remain consistent through out the code.

--- Thibedeau_Christopher_Functions_Assignment/js/script.js
+++ Thibedeau_Christopher_Functions_Assignment/js/script.js
@@ -5,8 +5,8 @@
 
 //Variables
 var playersName = prompt("Welcome to my store, what is your name?"); //Asks the player's name
-var userAge = prompt("How old are you, sorry have to make sure your old enough to play."); //Used to verify age of the user
-var powerBallChoice = prompt("Would you like numbers for Powerball, \nor for Florida State Lottery?"); //Ask's if the player which version of lotto the player would like.
+var playersAge; //= prompt("How old are you, sorry have to make sure your old enough to play."); //Used to verify age of the user
+var lotoTicketChoice; //= prompt("Would you like numbers for Powerball, \nor for Florida State Lottery?"); //Ask's if the player which version of lotto the player would like.
 
 //Functions
 function thanksForTellingMe(wholeName){ //Function for validating name
@@ -19,9 +19,27 @@ function thanksForTellingMe(wholeName){ //Function for validating name
         return wholeName; // Returns the value of the name back out of the Function thanksForTellingMe
 }
 
+function ageVerification(howOldAreYou){ //Function for validating age of player
+
+    howOldAreYou = parseInt(prompt("How old are you? You have to be at least 18 to play.")); //Asks you to give your age, also tells you what the minimum age is. It will convert the string to a number.
+
+    while(howOldAreYou <= 18){ //Request for players age, wont let them have a ticket with out it.
+
+        howOldAreYou = prompt("You need to be at least 18 to play, please try again."); //Apu is being silly and giving the player a second chance, or third to give A correct age.
+
+    }
+
+    return howOldAreYou; // Returns the age back to the function ageVerification so that it can be outputed later.
+}
 
 //Main Code
 playersName = thanksForTellingMe(playersName); //Stores the return from the Function thanksForTellingMe into the variable playersName
 
+playersAge = ageVerification(playersAge); //Sores the return from the function ageVerification in the variable playersAge.
+
+
 console.log("Welcome to Kwike-E-Mart Mr. " + playersName + ", my name is Apu."); //A little humor added to the project, this piece actual just is a thank you to the user for giving their name. A well as returns a name in kind.
+
+console.log("You are sticking with " + userAge + " as your final answer. Good here is your " + lotoTicketChoice + " thank you come again."); //This is Apu giving the player their ticket after verifying their age and asking them to come again.
+
 //Now you hear Apu's voice in your head as your reading. Enjoy.
