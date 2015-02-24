c316970fa22585cdedf0eb22b084d6fedbd81177
cthibedeau <cthibedeau@gmail.com>
2015-02-24 15:18:14 -0800

    Made a correction to the howOldAreYou var in the Function ageVerification. It was <=18 now it is <=17, the reasoning is that if someone was to put in 18 it would not let them continue. Since the legal age is 18 I made sure to change it to 17 to allow them to continue.

--- Thibedeau_Christopher_Functions_Assignment/js/script.js
+++ Thibedeau_Christopher_Functions_Assignment/js/script.js
@@ -23,7 +23,7 @@ function ageVerification(howOldAreYou){ //Function for validating age of player
 
     howOldAreYou = parseInt(prompt("How old are you? You have to be at least 18 to play.")); //Asks you to give your age, also tells you what the minimum age is. It will convert the string to a number.
 
-    while(howOldAreYou <= 18){ //Request for players age, wont let them have a ticket with out it.
+    while(howOldAreYou <= 17){ //Request for players age, wont let them have a ticket with out it.
 
         howOldAreYou = prompt("You need to be at least 18 to play, please try again."); //Apu is being silly and giving the player a second chance, or third to give A correct age.
 
@@ -40,6 +40,6 @@ playersAge = ageVerification(playersAge); //Sores the return from the function a
 
 console.log("Welcome to Kwike-E-Mart Mr. " + playersName + ", my name is Apu."); //A little humor added to the project, this piece actual just is a thank you to the user for giving their name. A well as returns a name in kind.
 
-console.log("You are sticking with " + userAge + " as your final answer. Good here is your " + lotoTicketChoice + " thank you come again."); //This is Apu giving the player their ticket after verifying their age and asking them to come again.
+console.log("You are sticking with " + playersAge + " as your final answer. Good here is your " + lotoTicketChoice + " thank you come again."); //This is Apu giving the player their ticket after verifying their age and asking them to come again.
 
 //Now you hear Apu's voice in your head as your reading. Enjoy.
