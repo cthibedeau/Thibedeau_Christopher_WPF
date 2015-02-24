ccca27245a20f5e1fd176caa973f25f7a853dc02
cthibedeau <cthibedeau@gmail.com>
2015-02-24 13:57:15 -0800

    Added in first Function, this piece is just to verify that a name was entered. If no name is entered it will continue to ask for one till it is.(Kind of annoying to play with people like that, but inquiring mind's want to know who they are talking to). Also set in comments for the function. I have also set up first stage of the main code , and added comments to that as well.

--- Thibedeau_Christopher_Functions_Assignment/js/script.js
+++ Thibedeau_Christopher_Functions_Assignment/js/script.js
@@ -4,8 +4,24 @@
 //SDI 0215
 
 //Variables
-
 var playersName = prompt("Welcome to my store, what is your name?"); //Asks the player's name
 var userAge = prompt("How old are you, sorry have to make sure your old enough to play."); //Used to verify age of the user
 var powerBallChoice = prompt("Would you like numbers for Powerball, \nor for Florida State Lottery?"); //Ask's if the player which version of lotto the player would like.
 
+//Functions
+function thanksForTellingMe(wholeName){ //Function for validating name
+
+    while(wholeName == ""){ //Request to make sure the user is giving there name, wont let them continue with out it.
+
+        wholeName = prompt("It is good luck to introduce your self."); //Just trying to encourage the user to be friendly.
+
+    }
+        return wholeName; // Returns the value of the name back out of the Function thanksForTellingMe
+}
+
+
+//Main Code
+playersName = thanksForTellingMe(playersName); //Stores the return from the Function thanksForTellingMe into the variable playersName
+
+console.log("Welcome to Kwike-E-Mart Mr. " + playersName + ", my name is Apu."); //A little humor added to the project, this piece actual just is a thank you to the user for giving their name. A well as returns a name in kind.
+//Now you hear Apu's voice in your head as your reading. Enjoy.
