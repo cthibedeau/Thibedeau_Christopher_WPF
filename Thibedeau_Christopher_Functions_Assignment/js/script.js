889e325d67be462742a3dfdedb0471377340d2c7
Ridd <cthibedeau@gmail.com>
2015-02-24 20:57:59 -0800

    Commented all of the code, also corrected the Florida State ensuring that it comes out with 6 numbers. Also ran the code a couple of times and put some of the numbers at the bottom of the code.

--- Thibedeau_Christopher_Functions_Assignment/js/script.js
+++ Thibedeau_Christopher_Functions_Assignment/js/script.js
@@ -32,40 +32,38 @@ function ageVerification(howOldAreYou){ //Function for validating age of player
     return howOldAreYou; // Returns the age back to the function ageVerification so that it can be outputted later.
 }
 
+function floridaStateLottery(floridaLottery){//Function for crating an array of numbers if Florida State lotto is picked.
 
+    var florida = []; //Sets up an empty array to store the numbers in.
 
-function floridaStateLottery(floridaLottery){
+            for(var i = 0; i <= 5; i++){ // Creates a cycle count that starts at 0 and goes up to 4 in the array, giving me 5 numbers.
 
-    var florida = [];
+                floridaLottery = Math.random() * (53 - 1) + (1); //Creates a random number between 1 and 53.
 
-            for(var i = 0; i <= 4; i++){
+                floridaLottery = Math.floor(floridaLottery); //Rounds the number down so that it is a whole number.
 
-                floridaLottery = Math.random() * (53 - 1) + (1);
-
-                floridaLottery = Math.floor(floridaLottery);
-
-                florida[i] = floridaLottery;
+                florida[i] = floridaLottery; //Tells the array to take the numbers from the random call.
 
             }
 
-    return florida;
+    return florida; //Returns the numbers back out.
 }
 
-function powerBallLottery(powerBall){
+function powerBallLottery(powerBall){//Function for crating an array of numbers if PowerBall lotto is picked.
 
-    var powerLotto = [];
+    var powerLotto = [];//Sets up an empty array to store the numbers in.
 
-            for(var h = 0; h <= 4; h++){
+            for(var h = 0; h <= 4; h++){// Creates a cycle count that starts at 0 and goes up to 4 in the array, giving me 5 numbers.
 
-                powerBall = Math.random() * (59 - 1) + (1);
+                powerBall = Math.random() * (59 - 1) + (1);//Creates a random number between 1 and 53.
 
-                powerBall = Math.floor(powerBall);
+                powerBall = Math.floor(powerBall);//Rounds the number down so that it is a whole number.
 
-                powerLotto[h] = powerBall;
+                powerLotto[h] = powerBall;//Tells the array to take the numbers from the random call.
 
             }
 
-    return powerLotto;
+    return powerLotto;//Returns the numbers back out.
 
 }
 
@@ -76,21 +74,25 @@ playersAge = ageVerification(); //Sores the return from the function ageVerifica
 
 console.log("Welcome to Kwike-E-Mart Mr. " + playersName + ", my name is Apu."); //A little humor added to the project, this piece actual just is a thank you to the user for giving their name. A well as returns a name in kind.
 
-if(lottoTicket == "Florida State"){
+if(lottoTicket == "Florida State"){//This is an if statement to see which option is picked Florida or PowerBall.
 
-   lottoTicket = floridaStateLottery(lottoTicket);
+   lottoTicket = floridaStateLottery(lottoTicket);//Calls the Function for Florida State lotto.
 
-    console.log("Your numbers for the Florida State Lottery is " + floridaStateLottery(lottoTicket) + " good luck. Thank you come again.");
+    console.log("Your numbers for the Florida State Lottery is " + floridaStateLottery(lottoTicket) + " good luck. Thank you come again.");//Returns a console log to the player if they choose Florida.
 
-}else{
+}else{//If Florida State lotto is not chosen then it will run the statement for PowerBall.
 
-   lottoTicket = powerBallLottery(lottoTicket);
+   lottoTicket = powerBallLottery(lottoTicket);//Stores the return from the Power Ball lotto.
 
-    var powerBallNumber  = Math.random() * (35 - 1) + (1);
+    var powerBallNumber  = Math.random() * (35 - 1) + (1);//Creates a random number for the Power Ball.
 
-    powerBallNumber = Math.floor(powerBallNumber);
+    powerBallNumber = Math.floor(powerBallNumber);//Rounds the number down so it is a whole number.
 
-    console.log("Your numbers for the Power Ball Lottery is " + powerBallLottery(lottoTicket) + " and you Power Ball Number is " + powerBallNumber + " luck. Thank you come again.");
+    console.log("Your numbers for the Power Ball Lottery is " + powerBallLottery(lottoTicket) + " and you Power Ball Number is " + powerBallNumber + " luck. Thank you come again.");//Returns a console log to the player if they choose Power Ball.
 }
 
-//Now you hear Apu's voice in your head as your reading. Enjoy.
+//Florida State numbers 7,14,3,40,24,18
+//PowerBall numbers 5,28,52,5,38 Powerball is 5
+
+
+//Now you hear Apu's voice in your head as your reading. Enjoy!
